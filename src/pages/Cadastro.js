06ae8e7a071d4c55fa5f5479797a9fc60eec604b
Mateus-Mantova <mateusdemantova@gmail.com>
2026-04-21 13:19:13 -0300

    Cadastro
    Use state

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Cadastro.css';
+import { useState } from 'react';
 
 function Cadastro() {
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    confirmarSenha: ""
+  });
+
+   const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if (!form.nome || !form.email || !form.senha || !form.confirmarSenha) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  if (form.senha !== form.confirmarSenha) {
+    alert("As senhas não coincidem!");
+    return;
+  }
+  alert("Cadastro realizado com sucesso!")
+
+  console.log(form);
+  };
+
   return (
     <main className="container auth-page">
       <section className="auth-box">
@@ -10,14 +40,16 @@ function Cadastro() {
           <h1>Criar Conta</h1>
           <p>Cadastre-se para começar a explorar o Brasil.</p>
         </div>
-
-        <form className="auth-form">
+   
+        <form className="auth-form" onSubmit={handleSubmit}>
           <div className="auth-field">
             <label htmlFor="nome">Nome</label>
             <input
               type="text"
               id="nome"
               name="nome"
+              value={form.nome}
+              onChange={handleChange}
               placeholder="Seu nome completo"
             />
           </div>
@@ -28,6 +60,8 @@ function Cadastro() {
               type="email"
               id="email"
               name="email"
+              value={form.email}
+              onChange={handleChange}
               placeholder="Seu e-mail"
             />
           </div>
@@ -38,6 +72,8 @@ function Cadastro() {
               type="password"
               id="senha"
               name="senha"
+              value={form.senha}
+              onChange={handleChange}
               placeholder="Crie uma senha"
             />
           </div>
@@ -47,7 +83,9 @@ function Cadastro() {
             <input
               type="password"
               id="confirmar-senha"
-              name="confirmar-senha"
+              name="confirmarSenha"
+              value={form.confirmarSenha}
+              onChange={handleChange}
               placeholder="Confirme sua senha"
             />
           </div>
